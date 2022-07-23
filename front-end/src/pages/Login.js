77f3fc209ab541d3b14a1890b7341f37fbb37067
Seonggwan Han <gkstjdrhks30@gmail.com>
2022-07-23 18:41:43 +0900

    login-page

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -6,45 +6,24 @@ function Login() {
 
   return(
     <div className = "login_container">
-      <div class="navbar">
-      <a href="#" id="logo">
-        <img src="images/hufs_logo2.jpg" width="144">
-      </a>
-      
-      <ul id="menu">
-        <li><a href="#">1</li>
-        <li><a href="#">2</li>
-        <li><a href="#">3</li>
-        <li><a href="#">4</li>
-      </ul>
-    </div>
-    <div class="main-header"></div>
-    
-    
-    <div class="products">
-      <h3>미정</h3>
-      
-      {<div class="product-list">
-        <a href="#" class="product">
-          <img src="images/sunglasses.jpg" width="225">
-          <div class="product-name">Sunglasses</div>
-          <div class="product-price">49,000</div>
-        </a>
-        
-        <a href="#" class="product">
-          <img src="images/tassel_loafer.jpg" width="225">
-          <div class="product-name">Tassel loafer</div>
-          <div class="product-price">89,000</div>
-        </a>
-          
-        <a href="#" class="product">
-          <img src="images/beige_bag.jpg" width="225">
-          <div class="product-name">Beige Bag</div>
-          <div class="product-price">69,000</div>
-        </a>
-        <div class="clearfix"></div>
+      <div class="center">
+        <h1>로그인</h1>
+        <form method="post">
+          <div class="txt_field">
+            <input type="text" required />
+            <span></span>
+            <label>Student Number</label>
+          </div>
+          <div class="txt_field">
+            <input type="password" required />
+            <span></span>
+            <label>Password</label>
+          </div>
+          <div class="pass">Forgot Password?</div>
+          <input type="submit" value="Login" />
+          <div class="signup_link">Not a member? <a href="#">Signup</a></div>
+        </form>
       </div>
-      </div>}
     </div>
   )
 }
