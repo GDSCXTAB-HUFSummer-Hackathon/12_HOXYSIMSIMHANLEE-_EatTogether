0487ef28a1f4cb42ba486b92da84b76278815308
JUNJOY08 <sjbssk0809@gmail.com>
2022-07-24 01:09:34 +0900

    서버구축

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState, useEffect }  from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import '@css/style.css';
+ 
+import BoardList from '@components/BoardList';
+import BoardNew from '@components/BoardNew';
+import BoardContent from '@components/BoardContent';
+import Footer from '@components/Footer';
+ 
+// axios 추가
+import axios from 'axios';
+ 
+function App () {
+ 
+	// 서버에서 받은 데이터를 console로 찍어서 확인한다.
+  useEffect(() => {
+    axios.get('/api/test')
+      .then(res => console.log(res))
+      .catch()
+  })
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Switch>
+            <Route path='/' component={BoardList} exact />
+            <Route path='/BoardNew' component={BoardNew} exact />
+            <Route path='/BoardContent' component={BoardContent} exact />
+          </Switch>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
-
+ 
 export default App;
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const PORT = process.env.port || 8000;
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "whs20102011",
+  database: "bbs",
+});
+
+app.get("/", (req, res) => {
+  const sqlQuery = "INSERT INTO requested (rowno) VALUES (2)";
+  db.query(sqlQuery, (err, result) => {
+    console.log(err);
+    res.send("success!");
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
